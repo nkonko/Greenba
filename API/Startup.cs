ff2f72bafd05e78f684be2159b2c9b4f19b956eb
Kevin Dickmeiss <kdickmeiss@kembit.nl>
2020-04-24 14:26:35 +0200

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -26,6 +27,13 @@ namespace API
 
       services.AddDbContext<StoreContext>(x =>
       x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddSingleton<IConnectionMultiplexer>(c =>
+      {
+        var configuration = ConfigurationOptions.Parse(_configuration
+            .GetConnectionString("Redis"), true);
+        return ConnectionMultiplexer.Connect(configuration);
+      });
 
       services.AddApplicationServices();
       services.AddSwaggerDocumentation();
---
+++ Core/Entities/CustomerBasket.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Core.Entities
+{
+  public class CustomerBasket
+  {
+    // For redis so that it can create a basket without out (and ran into problems)
+    public CustomerBasket()
+    {
+    }
+
+    public CustomerBasket(string id)
+    {
+      Id = id;
+    }
+
+    public string Id { get; set; }
+    public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+  }
+}
